b929ac43100f26c6624a64f4d9cd0128b430d175
jwagdav <jwagdav@gmail.com>
2022-03-21 20:11:16 -0700

    modified createGrid function to set a css variable equal to the funciton parameter. Successfully creates a grid as expected.

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 function createGrid(x){
 
     const container = document.querySelector('#container');
-
+    container.style.setProperty('--grid-size', x);
     
-    for(let i = 0; i <= x; i++) {
-        for(let j = 0; j <=x; j++) {
+    for(let i = 1; i <= x; i++) {
+        for(let j = 1; j <=x; j++) {
             const newDiv = document.createElement('div');
-            newDiv.classList.add('grid');
+            newDiv.classList.add('grid-items');
             container.appendChild(newDiv);
         }
     }
