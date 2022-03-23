ba1eea172038accc405d624a1b18d5b6abbf82ca
jwagdav <jwagdav@gmail.com>
2022-03-22 21:37:03 -0700

    added functionality to change background color of divs on mouseover

--- main.js
+++ main.js
@@ -2,15 +2,34 @@ function createGrid(x){
 
     const container = document.querySelector('#container');
     container.style.setProperty('--grid-size', x);
+
+    
     
     for(let i = 1; i <= x; i++) {
         for(let j = 1; j <=x; j++) {
             const newDiv = document.createElement('div');
             newDiv.classList.add('grid-items');
             container.appendChild(newDiv);
+
+            newDiv.addEventListener('mouseover', () => {
+                newDiv.style.backgroundColor = makeRGB();
+            });
+
+            
         }
     }
     
 }
 
-createGrid(16);
+function randomNum(num){
+    const randomNumber = Math.floor(Math.random() * (num + 1));
+    return randomNumber;
+}
+
+function makeRGB(){
+    let newRGB = `rgb(${randomNum(255)}, ${randomNum(255)}, ${randomNum(255)}, ${randomNum(100)})`;
+    return newRGB;
+}
+
+createGrid(4);
+
